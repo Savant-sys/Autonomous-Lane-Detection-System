ea1ef6461a0ecdbe5abf631a05f1608b23f8bc70
Michael Khuri <michaelkhuri@gmail.com>
2025-12-12 18:31:17 -0800

    Add histogram

--- Sliding Windows/LaneDetection.py
+++ Sliding Windows/LaneDetection.py
@@ -1,9 +1,21 @@
 import cv2
-import numpy as np  
+import numpy as np
 
 # vidcap = cv2.VideoCapture('/Users/michaelkhuri/Desktop/Autonomous-Lane-Detection-System/videos/testdashcam.mov')
 vidcap = cv2.VideoCapture('/Users/michaelkhuri/Desktop/Autonomous-Lane-Detection-System/videos/LaneVideo.mp4')
 success, image = vidcap.read()
+
+def nothing(a):
+    pass
+
+cv2.namedWindow("Trackbars")
+
+cv2.createTrackbar("L - H", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 200, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 50, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
 
 while success:
     success, image = vidcap.read()
@@ -29,7 +41,7 @@ while success:
     transformed_frame = cv2.warpPerspective(frame, matrix, (640, 480))
 
     ### Object Detection ###
-    hsv_transformed_frame = cv2.cvtColor(transformed_frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(transformed_frame, cv2.COLOR_BGR2HSV)
 
     l_h = cv2.getTrackbarPos("L - H", "Trackbars")
     l_s = cv2.getTrackbarPos("L - S", "Trackbars")
@@ -40,17 +52,25 @@ while success:
 
     lower = np.array([l_h, l_s, l_v])
     upper = np.array([u_h, u_s, u_v])
-    mask = cv2.inRange(hsv_transformed_frame, lower, upper)
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Histogram
+    histogram = np.sum(mask[mask.shape[0]//2:, :], axis=0)
+    midpoint = np.int64(histogram.shape[0]/2)
+    left_base = np.argmax(histogram[:midpoint])
+    right_base = np.argmax(histogram[midpoint:]) + midpoint
 
     # move windows (for macOS)
-    cv2.moveWindow("Original", 0, -100)
-    cv2.moveWindow("Transformed", 650, -100)
-    cv2.moveWindow("Lane Detection - Image Thresholding", 800, 250)
+    # cv2.moveWindow("Original", 0, 200)
+    # cv2.moveWindow("Transformed", 650, -100)
+    # cv2.moveWindow("Lane Detection - Image Thresholding", 800, 250)
+    # cv2.moveWindow("Trackbars", 1200, 0)
 
     # Display the frames
     cv2.imshow("Original", frame)
+    # cv2.imshow("HSV", midpoint)
     cv2.imshow("Transformed", transformed_frame)
-    cv2.imshow("Mask", mask)
+    cv2.imshow("Lane Detection - Image Thresholding", mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
